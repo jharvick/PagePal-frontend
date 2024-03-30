429e3b4386e370e1f957e31b0cc18441962f59cc
Jared Harvick <jharvick38@gmail.com>
2024-03-30 12:39:52 -0500

    Set up initial data and added an ItemsIndex action

---
+++ src/ItemsIndex.jsx
@@ -0,0 +1,15 @@
+export function ItemsIndex(props) {
+  return (
+    <div>
+      <h1>All items</h1>
+      {props.items.map((item) => (
+        <div key={item.name}>
+          <h2>Title: {item.name}</h2>
+          <img src={item.image_url} alt={item.name} />
+          <p>Description: {item.description}</p>
+          <p>Category: {item.category}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
